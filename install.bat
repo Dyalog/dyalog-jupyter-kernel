7cee2063606ee8669f09952a0d6b29d75244b277
RikedyP <rikedyp@users.noreply.github.com>
2023-07-13 11:41:50 +0100

    get site-packages from running python

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @echo off
+set CMD=python3 -c "import site; sp=site.getsitepackages(); print(sp[1])"
+for /F "tokens=* USEBACKQ" %%F in (`%CMD%`) do (set SITE_PACKAGES=%%F)
+echo %SITE_PACKAGES%
 xcopy %0\..\dyalog-kernel %APPDATA%\jupyter\kernels\dyalog-kernel /Y /I
-xcopy %0\..\dyalog_kernel %APPDATA%\Python\Python36\site-packages\dyalog_kernel /Y /I
-xcopy %0\..\dyalog_kernel %USERPROFILE%\AppData\Local\Continuum\anaconda3\Lib\site-packages\dyalog_kernel /Y /I
-xcopy %0\..\dyalog_kernel %USERPROFILE%\anaconda3\Lib\site-packages\dyalog_kernel /Y /I
-xcopy %0\..\dyalog_kernel %USERPROFILE%\Miniconda3\Lib\site-packages\dyalog_kernel /Y /I
+xcopy %0\..\dyalog_kernel %SITE_PACKAGES%\dyalog_kernel /Y /I
+
