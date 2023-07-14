2efe8744c236cc581e203007486ce1fde2475912
RikedyP <rikedyp@users.noreply.github.com>
2023-07-14 10:36:06 +0100

    fix get site-package location

--- install.bat
+++ install.bat
@@ -1,7 +1,6 @@
 @echo off
-set CMD=python -c "import site; sp=site.getsitepackages(); print(sp[1])"
+set CMD=python -m site --user-site
 for /F "tokens=* USEBACKQ" %%F in (`%CMD%`) do (set SITE_PACKAGES=%%F)
 echo %SITE_PACKAGES%
 xcopy %0\..\dyalog-kernel %APPDATA%\jupyter\kernels\dyalog-kernel /Y /I
-xcopy %0\..\dyalog_kernel %SITE_PACKAGES%\dyalog_kernel /Y /I
-
+xcopy %0\..\dyalog_kernel "%SITE_PACKAGES%\dyalog_kernel" /Y /I
